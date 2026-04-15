74e44e0f1894fe7e96dcfbea2432132e124aded5
baasnjargalhhho-alt <baasnjargalhhho@gmail.com>
2026-04-15 18:07:32 +0800

    Fix bugs and improve comments in ArrayIntQueue
    Refactor ArrayIntQueue with bug fixes and comments.

--- Java/src/main/java/edu/cmu/cs/cs214/rec02/ArrayIntQueue.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec02/ArrayIntQueue.java
@@ -2,107 +2,112 @@ package edu.cmu.cs.cs214.rec02;
 
 import java.util.Arrays;
 
-/**
- * A resizable-array implementation of the {@link IntQueue} interface. The head of
- * the queue starts out at the head of the array, allowing the queue to grow and
- * shrink in constant time.
- *
- * TODO: This implementation contains three bugs! Use your tests to determine the
- * source of the bugs and correct them!
- *
- * @author Alex Lockwood
- * @author Ye Lu
- */
 public class ArrayIntQueue implements IntQueue {
 
-    /**
-     * An array holding this queue's data
-     */
+    // Queue-д ашиглах массив
     private int[] elementData;
 
-    /**
-     * Index of the next dequeue-able value
-     */
+    // Queue-ийн эхний элемент (dequeue хийх байр)
     private int head;
 
-    /**
-     * Current size of queue
-     */
+    // Одоогийн элементүүдийн тоо
     private int size;
 
-    /**
-     * The initial size for new instances of ArrayQueue
-     */
     private static final int INITIAL_SIZE = 10;
 
-    /**
-     * Constructs an empty queue with an initial capacity of ten.
-     */
+    // Конструктор
     public ArrayIntQueue() {
         elementData = new int[INITIAL_SIZE];
         head = 0;
         size = 0;
     }
 
-    /** {@inheritDoc} */
+    // Queue-г цэвэрлэх
     public void clear() {
-        Arrays.fill(elementData, 0);
-        size = 0;
-        head = 0;
+        Arrays.fill(elementData, 0); // массивыг 0 болгоно
+        size = 0;   // элемент байхгүй
+        head = 0;   // эхлэл буцаана
     }
 
-    /** {@inheritDoc} */
+    // Элемент гаргах (FIFO)
     public Integer dequeue() {
+
+        // Хоосон бол null буцаана
         if (isEmpty()) {
             return null;
         }
+
+        // head дээрх утгыг авна
         Integer value = elementData[head];
+
+        // head-ийг дараагийн байр руу шилжүүлнэ (circular)
         head = (head + 1) % elementData.length;
+
+        // size багасна
         size--;
+
         return value;
     }
 
-    /** {@inheritDoc} */
+    // Элемент нэмэх
     public boolean enqueue(Integer value) {
+
+        // Хэрвээ дүүрсэн бол capacity нэмнэ
         ensureCapacity();
+
+        // tail = дараагийн нэмэх индекс
         int tail = (head + size) % elementData.length;
+
+        // утга хадгална
         elementData[tail] = value;
+
+        // size нэмэгдэнэ
         size++;
+
         return true;
     }
 
-    /** {@inheritDoc} */
+    // ❗ ЗАСВАР: зөв хоосон шалгалт
     public boolean isEmpty() {
-        return size >= 0;
+        return size == 0;
     }
 
-    /** {@inheritDoc} */
+    // ❗ ЗАСВАР: хоосон үед null
     public Integer peek() {
+        if (isEmpty()) {
+            return null;
+        }
         return elementData[head];
     }
 
-    /** {@inheritDoc} */
+    // Одоогийн size
     public int size() {
         return size;
     }
 
-    /**
-     * Increases the capacity of this <tt>ArrayIntQueue</tt> instance, if
-     * necessary, to ensure that it can hold at least size + 1 elements.
-     */
+    // ❗ ЗАСВАР: зөв copy хийдэг болгов
     private void ensureCapacity() {
+
         if (size == elementData.length) {
+
             int oldCapacity = elementData.length;
             int newCapacity = 2 * oldCapacity + 1;
+
             int[] newData = new int[newCapacity];
-            for (int i = head; i < oldCapacity; i++) {
-                newData[i - head] = elementData[i];
+
+            // 🔥 ЗӨВ COPY (дарааллаар)
+            for (int i = 0; i < size; i++) {
+                newData[i] = elementData[(head + i) % oldCapacity];
             }
-            for (int i = 0; i < head; i++) {
-                newData[head - i] = elementData[i];
-            }
+
+            // шинэ массив руу шилжүүлнэ
             elementData = newData;
+
+            // head-г 0 болгоно (straight array боллоо)
             head = 0;
+        }
+    }
+}            head = 0;
         }
     }
 }
